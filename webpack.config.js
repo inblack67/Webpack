cfe27c9c05c7818260590e18140e13e803d6db18
Aman <devproddeploy@gmail.com>
2020-07-07 20:38:10 +0530

    content hashing and loading html through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'development',    // no gibrish
     // devtools: 'none',    // removes eval
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        // to prevent cash busting, random name every time more like snapshots
+        filename: 'main.[contentHash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/template.html'
+    })],
     module: {
         rules: [
             {
